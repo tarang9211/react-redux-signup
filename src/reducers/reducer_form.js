51e3e79d9a7dd99e24976b333c066647abc9eef0
Tarang Hirani <tarang9211@gmail.com>
2017-04-02 20:06:22 +0530

    fixed spacing

--- src/reducers/reducer_form.js
+++ src/reducers/reducer_form.js
@@ -9,11 +9,11 @@ const initialState = {
 export default function (state = initialState, action) {
   switch(action.type) {
     case INCREMENT_STEP:
-      return {...state,
+      return {
+        ...state,
         step: state.step + 1
       };
     default:
       return state;
-
   }
 }
