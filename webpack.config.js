861a2eef06cd130b2544ac6ef48e4387dd97198a
Tarang Hirani <tarang9211@gmail.com>
2017-04-02 20:05:14 +0530

    updated webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,17 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: './src/index.js',
   output: {
     path: __dirname,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ]
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './'
   }
-};
+}
